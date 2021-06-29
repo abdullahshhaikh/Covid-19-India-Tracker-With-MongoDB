164fd1db784cd1dc30a03e95406afc915be9f110
Abdullah <>
2021-06-29 14:09:36 +0530

    Minor changes

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0.02vkl.mongodb.net/covid-tracker-api", {
     useNewUrlParser: true,
